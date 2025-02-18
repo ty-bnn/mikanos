5ffa5a4f55478583413a2d26e0d113f6a98ba071
ty-bnn <takayuki.yamamoto.dev@gmail.com>
2025-02-18 05:09:08 +0000

    Delete echo command from edk2_setup.sh

--- edk2_setup.sh
+++ edk2_setup.sh
@@ -5,7 +5,6 @@ EDK2_DIR="$HOME/edk2"
 EDK2_ARM_DIR="$SRC_DIR/edk2"
 
 # edk2のセットアップ
-echo $EDK2_DIR/edksetup.sh
 source $EDK2_DIR/edksetup.sh
 
 # edk2のビルド時のターゲット設定を上書きする
@@ -14,7 +13,10 @@ cp $EDK2_ARM_DIR/Conf/target.txt $EDK2_DIR/Conf/target.txt
 # edk2の設定をApple Silicon用に上書きする
 cp $EDK2_ARM_DIR/Conf/tools_def.txt $EDK2_DIR/Conf/tools_def.txt
 
-# ブートローダーのディレクトリをedk2側にリンクしてビルドできるようにする
-if [ ! -e "$EDK2_DIR/MikanLoaderPkg" ]; then
-    ln -s $SRC_DIR/$1/MikanLoaderPkg $EDK2_DIR
+# 既に存在するedk2/MikanLoaderPkgのリンク削除する
+if [ -e "$EDK2_DIR/MikanLoaderPkg" ]; then
+    unlink $EDK2_DIR/MikanLoaderPkg
 fi
+
+# ブートローダーのディレクトリをedk2側にリンクしてビルドできるようにする
+ln -s $SRC_DIR/$1/MikanLoaderPkg $EDK2_DIR
