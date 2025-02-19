81c7a09f8451b91fa5603cd897c84262ede38f0c
ty-bnn <takayuki.yamamoto.dev@gmail.com>
2025-02-19 09:09:59 +0000

    Add rm MikanLoaderPkg in edk2 for edk2_setup.sh

--- edk2_setup.sh
+++ edk2_setup.sh
@@ -5,7 +5,9 @@ EDK2_DIR="$HOME/edk2"
 EDK2_ARM_DIR="$SRC_DIR/edk2"
 
 # edk2のセットアップ
+cd $EDK2_DIR
 source $EDK2_DIR/edksetup.sh
+cd $SRC_DIR
 
 # edk2のビルド時のターゲット設定を上書きする
 cp $EDK2_ARM_DIR/Conf/target.txt $EDK2_DIR/Conf/target.txt
@@ -14,8 +16,9 @@ cp $EDK2_ARM_DIR/Conf/target.txt $EDK2_DIR/Conf/target.txt
 cp $EDK2_ARM_DIR/Conf/tools_def.txt $EDK2_DIR/Conf/tools_def.txt
 
 # 既に存在するedk2/MikanLoaderPkgのリンク削除する
-if [ -e "$EDK2_DIR/MikanLoaderPkg" ]; then
+if [ -d "$EDK2_DIR/MikanLoaderPkg" ]; then
     unlink $EDK2_DIR/MikanLoaderPkg
+    rm -rf $EDK2_DIR/MikanLoaderPkg
 fi
 
 # ブートローダーのディレクトリをedk2側にリンクしてビルドできるようにする
