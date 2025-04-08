ae90a0313056444979f21a6cb359a5aa60e3cb6a
ty-bnn <takayuki.yamamoto.dev@gmail.com>
2025-04-08 15:19:55 +0000

    day06a

--- aoi/kernel/graphics.hpp
+++ aoi/kernel/graphics.hpp
@@ -24,7 +24,6 @@ class PixelWriter {
   const FrameBufferConfig& config_;
 };
 
-// #@@range_begin(pixel_writer_def)
 class RGBResv8BitPerColorPixelWriter : public PixelWriter {
  public:
   using PixelWriter::PixelWriter;
@@ -36,4 +35,14 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
   using PixelWriter::PixelWriter;
   virtual void Write(int x, int y, const PixelColor& c) override;
 };
-// #@@range_end(pixel_writer_def)
+
+template <typename T>
+struct Vector2D {
+  T x, y;
+};
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c);
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c);
