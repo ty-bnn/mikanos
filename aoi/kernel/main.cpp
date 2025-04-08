fd0f6e4ae56456706689b6ccbc45c0cbbca4ea9f
ty-bnn <takayuki.yamamoto.dev@gmail.com>
2025-04-08 16:07:42 +0000

    day06b

--- aoi/kernel/main.cpp
+++ aoi/kernel/main.cpp
@@ -12,13 +12,7 @@
 #include "graphics.hpp"
 #include "font.hpp"
 #include "console.hpp"
-
-/**
- * 配置newの宣言
- */
-void* operator new(size_t size, void* buf) {
-  return buf;
-}
+#include "pci.hpp"
 
 /**
  * deleteを宣言しないとコンパイルエラー
@@ -131,6 +125,18 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
       }
     }
+  }
+
+  auto err = pci::ScanAllBus();
+  printk("ScanAllBus: %s\n", err.Name());
+
+  for (int i = 0; i < pci::num_device; ++i) {
+    const auto& dev = pci::devices[i];
+    auto vendor_id = pci::ReadVendorId(dev.bus, dev.device, dev.function);
+    auto class_code = pci::ReadClassCode(dev.bus, dev.device, dev.function);
+    printk("%d.%d.%d: vend %04x, class %08x, head %02x\n",
+        dev.bus, dev.device, dev.function,
+        vendor_id, class_code, dev.header_type);
   }
 
   while (1) __asm__("hlt");
