21bc8f4f8c504b9a610515c5449089cf293c0087
ty-bnn <takayuki.yamamoto.dev@gmail.com>
2025-06-15 15:07:38 +0000

    day09c

--- aoi/kernel/error.hpp
+++ aoi/kernel/error.hpp
@@ -27,6 +27,7 @@ class Error {
     kUnknownXHCISpeedID,
     kNoWaiter,
     kNoPCIMSI,
+    kUnknownPixelFormat,
     kLastOfCode,  // この列挙子は常に最後に配置する
   };
 
@@ -52,6 +53,7 @@ class Error {
     "kInvalidPhase",
     "kUnknownXHCISpeedID",
     "kNoWaiter",
+    "kUnknownPixelFormat",
     "kNoPCIMSI",
   };
   static_assert(Error::Code::kLastOfCode == code_names_.size());
